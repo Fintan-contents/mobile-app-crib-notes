5beb5273749d1bc4c9528aca850e5d9edd1b82ec
Takuto Inoue <inoue.takuto@tis.co.jp>
2020-09-28 03:50:48 +0000

    公開前の微修正

--- santoku-app/src/app/components/pages/top/Top.tsx
+++ santoku-app/src/app/components/pages/top/Top.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Container, Title, Text } from '../../basics';
 
 const Top: React.FC = () => {
   return (
     <Container>
-      <Title>ツールを決める、ときの考え方</Title>
-      <Text>・1本目の包丁として三徳包丁を（ふつうは）選ぶように、我々も1つめのツールとして選ぶ</Text>
-      <Text>
-        ・三徳包丁でクエを捌くのが現実的じゃなくて出刃包丁が必要になるように、我々のツールで捌けない案件なら別のツールの使い方を学ぶしかないと割り切る
+      <Title>モバイルアプリケーションの認証</Title>
+      <Text style={styles.text}>
+        モバイルアプリケーションは多種多様ですが、多くのアプリケーションでユーザ認証は必要になります。
+        ここでは、ユーザ認証ついてプロジェクトで参考にできる、サンプルアプリケーションのソースコードを用意しています。{'\n\n'}
+        ソースコードを参照して、開発者が実際の開発を進めやすいようにするのが目的です。{'\n\n'}
+        サンプルアプリケーションではReact Nativeを使っています。{'\n'}
+        そのためReact Nativeを使うプロジェクトで同じユースケースであればソースコードを流用可能です。
       </Text>
-      <Title>そう考えたとしても、決め手に欠ける</Title>
-      <Text>・関孫六がいいのかヘンケルスがいいのか</Text>
-      <Text>・実際（三徳包丁としては）どれでもいいはずで、結局は好みだと思う なので、自分たちが惚れ込めるツールを選ぶ</Text>
     </Container>
   );
 };
 
 export default Top;
+
+const styles = StyleSheet.create({
+  text: {
+    marginLeft: 10,
+  },
+});
