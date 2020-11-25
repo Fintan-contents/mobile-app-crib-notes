a47adbddf17e2bef1d4bae62f63732369e517bcf
Takumon <inouetakumon@gmail.com>
2020-11-25 19:44:08 +0900

    🐛:  KeyStoreとKeychainに関する説明を修正 (#13)
    Co-authored-by: In a Paddy <44867563+in-a-paddy@users.noreply.github.com>

--- santoku-app/src/app/components/pages/auth/StatelessAuth.tsx
+++ santoku-app/src/app/components/pages/auth/StatelessAuth.tsx
@@ -52,7 +52,7 @@ const StatelessAuthInner: React.FC = () => {
                 取得結果を以下に示します。AWS CognitoのOIDC認証ではIDトークン、アクセストークン、リフレッシュトークンの3つが取得できます。{'\n'}
                 ここでは便宜上取得結果を画面に表示していますが、
                 通常のアプリであれば、ユーザーまたは第三者の目に触れることのないようにセキュアなストレージに保管して管理してください。
-                このアプリでは、AndroidではKeyStore、iOSではKeychainに保管しています。
+                このアプリでは、AndroidではKeyStoreに保存した鍵で暗号化してSharedPreferenceに保管し、iOSではKeychainに保管しています。
               </Description>
               <Section>
                 <TextButton onPress={signOut} value={'サインアウト'} />
