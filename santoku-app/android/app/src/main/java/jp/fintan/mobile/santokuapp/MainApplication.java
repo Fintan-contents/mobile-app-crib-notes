39a1f750aeff2374145d07d962f062d3275418cf
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-11-27 17:22:38 +0900

    Revert "✨: Update expo sdk 39 (#7)"
    This reverts commit 3599b34add1805caef4b3aee1b232d089e3a9063.

--- santoku-app/android/app/src/main/java/jp/fintan/mobile/santokuapp/MainApplication.java
+++ santoku-app/android/app/src/main/java/jp/fintan/mobile/santokuapp/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-	import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -58,28 +57,23 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
   }
 
-	/**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  /**
+   * Loads Flipper in React Native templates.
    *
    * @param context
-   * @param reactInstanceManager
    */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
+  private static void initializeFlipper(Context context) {
     if (BuildConfig.DEBUG) {
       try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-	      Class<?> aClass = Class.forName("jp.fintan.mobile.santokuapp.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
       } catch (NoSuchMethodException e) {
