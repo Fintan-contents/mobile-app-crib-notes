dd3d7de7d0fbea2fb8f60cb4543bfb33490f342b
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-02-15 20:41:06 +0900

    🖋: Add pages to learn React Native, configure sidebars and fix links (#42)
    ## ✅ What's done
    
    - [x] 学習用コンテンツページを移動

---
+++ docs/react-native/learn/todo-app/app-spec.md
@@ -0,0 +1,57 @@
+---
+title: ToDoアプリの仕様
+---
+
+## 機能
+
+このハンズオンで作成するToDoアプリは、ToDoを管理するための次の機能を実現します。
+
+ - ToDoを登録できる
+ - 登録しているToDoを一覧形式で表示できる
+ - ToDoを完了にできる
+ - ToDoの完了を取り消せる
+ - 表示するToDoを絞り込むことができる
+ - ToDoを削除できる[^1]
+
+アプリを利用するためには、アカウントを登録してユーザを認証する必要があります。
+ただし、現在のバージョンではアプリのメモリ上にしか情報をもたないため、認証はダミーとします。
+
+[^1]: サンプルおよびガイドはありません。ヒントを参考にして実装してみてください。
+
+## 画面構成・デザイン
+
+画面構成とデザインは次のとおりです。
+
+### 画面遷移
+
+![画面遷移](app-spec/screen_transition.png)
+
+### Welcome
+
+![welcome](app-spec/welcome.png)
+
+### ユーザ登録
+
+![signup](app-spec/signup.png)
+
+### ログイン
+
+![login](app-spec/login.png)
+
+### ToDo一覧
+
+![todo_board_all](app-spec/todo_board_all.png)
+
+### ToDo作成
+
+![add_task](app-spec/add_task.png)
+
+### 設定Tab（ログアウト）
+
+![logout](app-spec/setting.png)
+
+## アプリの前提、制約
+
+ReactとReact Nativeの違いを体験することを主眼としており、現在のバージョンのToDoアプリはバックエンドと接続しません。また、アプリ内にもデータを保存しません。
+
+そのため、ToDoの一覧やToDoの更新はできますが、アプリを停止すると初期状態に戻ります。
--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,49 +1,101 @@
 module.exports = {
   reference: {
-    "このドキュメントについて": [
-      "reference/index"
-    ],
-    "セキュリティ": [
-      'reference/security/awesome-sites'
-    ],
-    "認証": [
+    このドキュメントについて: ['reference/index'],
+    セキュリティ: ['reference/security/awesome-sites'],
+    認証: [
       'reference/auth/authn-architecture',
       'reference/auth/login-screens',
       'reference/auth/manage-credentials',
       'reference/auth/stateless-authn',
-      'reference/auth/webassets-integration'
+      'reference/auth/webassets-integration',
     ],
-    "プッシュ通知": ['doc1', 'doc2', 'doc3']
+    プッシュ通知: ['doc1', 'doc2', 'doc3'],
   },
   guide: {
-    "About": [
-      'react-native/guide'
-    ],
-    "環境構築": [
-      'react-native/guide/environment-setup',
-    ],
-    "画面遷移": [
-      'react-native/guide/screen-transitions',
-    ]
+    About: ['react-native/guide'],
+    環境構築: ['react-native/guide/environment-setup'],
+    画面遷移: ['react-native/guide/screen-transitions'],
   },
-  learn: {
-    "About": [
-      'react-native/learn'
-    ],
-    "Getting Started": [
-      'react-native/learn/getting-started',
-      'react-native/learn/getting-started/setting-up-development-environment',
-      'react-native/learn/getting-started/create-project',
-      'react-native/learn/getting-started/launch-created-app',
-    ]
-  },
+  learn: [
+    {
+      type: 'doc',
+      id: 'react-native/learn',
+    },
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: [
+        'react-native/learn/getting-started',
+        'react-native/learn/getting-started/setting-up-development-environment',
+        'react-native/learn/getting-started/create-project',
+        'react-native/learn/getting-started/launch-created-app',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'アプリの実装前に',
+      items: [
+        'react-native/learn/basic-concepts',
+        'react-native/learn/basic-concepts/pre-requisites',
+        {
+          type: 'category',
+          label: 'React Nativeの基本',
+          items: [
+            'react-native/learn/basic-concepts/react-native-basics',
+            {
+              type: 'category',
+              label: 'Components',
+              items: [
+                'react-native/learn/basic-concepts/react-native-basics/components',
+                'react-native/learn/basic-concepts/react-native-basics/components/view',
+                'react-native/learn/basic-concepts/react-native-basics/components/text',
+                'react-native/learn/basic-concepts/react-native-basics/components/button',
+                'react-native/learn/basic-concepts/react-native-basics/components/touchable-opacity',
+                'react-native/learn/basic-concepts/react-native-basics/components/image',
+                'react-native/learn/basic-concepts/react-native-basics/components/scroll-view',
+                {
+                  type: 'category',
+                  label: 'Lists',
+                  items: [
+                    'react-native/learn/basic-concepts/react-native-basics/components/lists',
+                    'react-native/learn/basic-concepts/react-native-basics/components/lists/flat-list',
+                    'react-native/learn/basic-concepts/react-native-basics/components/lists/section-list',
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'category',
+              label: 'Design',
+              items: [
+                'react-native/learn/basic-concepts/react-native-basics/design',
+                'react-native/learn/basic-concepts/react-native-basics/design/style',
+                'react-native/learn/basic-concepts/react-native-basics/design/height-and-width',
+                'react-native/learn/basic-concepts/react-native-basics/design/flexbox',
+              ],
+            },
+          ],
+        },
+        {
+          type: 'category',
+          label: 'React Navigationの基本',
+          items: [
+            'react-native/learn/basic-concepts/react-navigation-basics',
+            'react-native/learn/basic-concepts/react-navigation-basics/stack',
+            'react-native/learn/basic-concepts/react-navigation-basics/modal',
+            'react-native/learn/basic-concepts/react-navigation-basics/tab',
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'ToDoアプリの実装',
+      items: ['react-native/learn/todo-app', 'react-native/learn/todo-app/app-spec'],
+    },
+  ],
   troubleshoot: {
-    "About": [
-      'react-native/troubleshooting'
-    ],
-    "アプリを実行できない": [
-      'react-native/troubleshooting/unable-to-load-script',
-      'react-native/troubleshooting/debug-keystore-not-found'
-    ],
+    About: ['react-native/troubleshooting'],
+    アプリを実行できない: ['react-native/troubleshooting/unable-to-load-script', 'react-native/troubleshooting/debug-keystore-not-found'],
   },
 };
