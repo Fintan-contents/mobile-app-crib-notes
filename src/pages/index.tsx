70a627781b3ffae58b33ab782c4450a49f6bcf81
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-03-01 11:44:30 +0900

    🚨: Fix EditorConfig max line length setting (#60)
    ## ✅ What's done
    
    - [x] EditorConfigの`max_line_length`を`120`に修正
    
    ## ⏸ What's not done
    
    - `santoku-app`内の設定は修正していません

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,9 @@ function Home() {
           <h1 className="hero__title">{siteConfig?.title || ''}</h1>
           <p className="hero__subtitle">{siteConfig?.tagline || ''}</p>
           <div className={styles.buttons}>
-            <Link className={clsx('button button--secondary button--lg', styles.getStarted)} to={useBaseUrl('react-native/learn')}>
+            <Link
+              className={clsx('button button--secondary button--lg', styles.getStarted)}
+              to={useBaseUrl('react-native/learn')}>
               Get Started
             </Link>
           </div>
