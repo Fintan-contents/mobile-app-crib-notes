b1a5ddb23c3c1b08a20f993f19ea0cfe246b5708
tani-takanori <74525873+tani-takanori@users.noreply.github.com>
2021-03-17 18:46:27 +0900

    ドキュメントの実装に合わせる (#97)
    ## 💣 BREAKING CHANGES 💣
    
    - 通知のメッセージをわかりやすくした
    
    ## ✅ What's done
    
    - [x] ドキュメントの内容に合わせて実装
    - [x] AndroidEmulatorで動作確認

--- santoku-app/src/app/components/pages/notification/PushNotification.tsx
+++ santoku-app/src/app/components/pages/notification/PushNotification.tsx
@@ -1,48 +1,54 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Container, Description, KeyboardAvoidingView} from '../../basics';
 import DeviceTokenNotificationForm from '../../parts/notification/DeviceTokenNotificationForm';
 import TopicNotificationForm from '../../parts/notification/TopicNotificationForm';
 import {useIsMounted} from '../../../../framework/hooks/useIsMounted';
 import pushNotificationService from '../../../backend/notification/PushNotificationService';
-import messaging, {FirebaseMessagingTypes} from '@react-native-firebase/messaging';
+import messaging from '@react-native-firebase/messaging';
 import {Alert} from 'react-native';
 
 const PushNotification: React.FC = () => {
   const [deviceToken, setDeviceToken] = useState<string>();
   const isMounted = useIsMounted();
-  const [recieveData, setRecieveData] = useState<string>();
-
-  const setRemoteMessage = useCallback(
-    (remoteMessage: FirebaseMessagingTypes.RemoteMessage | null) => {
-      if (remoteMessage && isMounted()) {
-        setRecieveData(JSON.stringify({...remoteMessage.notification, data: remoteMessage.data}));
-      }
-    },
-    [isMounted],
-  );
 
   useEffect(() => {
-    if (deviceToken) {
-      messaging().onNotificationOpenedApp(setRemoteMessage);
-      messaging().getInitialNotification().then(setRemoteMessage);
-      return messaging().onMessage((message) => {
-        Alert.alert('受信', JSON.stringify({...message.notification, data: message.data}));
+    if (!deviceToken) {
+      return;
+    }
+    // アプリ起動後、アプリが非アクティブな時にメッセージを受信した場合の処理を登録
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      Alert.alert(
+        'アプリが非アクティブな間にリモート通知を受信しました',
+        JSON.stringify({
+          notification: remoteMessage.notification,
+          data: remoteMessage.data,
+        }),
+      );
+    });
+    // アプリを起動する前に受信していたメッセージがあったか確認
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          Alert.alert(
+            'アプリが起動されていない間にリモート通知を受信しました',
+            JSON.stringify({
+              notification: remoteMessage.notification,
+              data: remoteMessage.data,
+            }),
+          );
+        }
       });
-    }
-  }, [deviceToken, isMounted, setRemoteMessage]);
-
-  useEffect(() => {
-    if (recieveData) {
-      Alert.alert('受信', recieveData, [
-        {
-          text: 'OK',
-          onPress: () => {
-            setRecieveData(undefined);
-          },
-        },
-      ]);
-    }
-  }, [recieveData]);
+    return messaging().onMessage((message) => {
+      Alert.alert(
+        'アプリの起動中にリモート通知を受信しました',
+        JSON.stringify({
+          notification: message.notification,
+          data: message.data,
+        }),
+      );
+    });
+  }, [deviceToken]);
 
   useEffect(() => {
     if (deviceToken) {
