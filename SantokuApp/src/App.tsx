54fa9babc7f372100c378552cbd33fbb3a6758cc
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-04-13 09:55:55 +0900

    🌱: Use `expo-keep-awake` while running in development mode (#158)
    ## ✅ What's done
    
    - [x] 開発中は、`expo-keep-awake`を利用して画面がスリープしないように設定

--- SantokuApp/src/App.tsx
+++ SantokuApp/src/App.tsx
@@ -1,8 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {activateKeepAwake} from 'expo-keep-awake';
 import {RootStackNav} from 'navigation';
 import React from 'react';
 
 export const App = () => {
+  // 開発中は画面がスリープしないようにしておきます。
+  if (__DEV__) {
+    activateKeepAwake();
+  }
+
   return (
     <NavigationContainer>
       <RootStackNav />
