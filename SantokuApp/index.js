96be9e222bcce59588c65dbaa1bdf74d78215c14
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-05-20 15:59:14 +0900

    ⬆: Upgrade Expo SDK to 41 (#232)
    ## ✅ What's done
    
    - [x] Expo SDK 41にアップデート
      - `expo upgrade`で関連ライブラリを更新
      - Android 11をターゲットにするようにビルド構成を更新
      - Android Gradleプラグインを3.5.3から4.1.0に更新
      - Gradleのバージョンを6.3から6.8に更新
      - `react-native-screens`が3.0.0に更新されたため、不要となった`enableScreens`を削除
      - CI環境（環境変数`CI`が設定された環境）でPodsをインストールしたときはFlipperを無効にするように変更
      - Metro Bundlerの設定にexpo/metro-configを使うように変更
      - TypeScriptの設定にExpoのデフォルト設定を反映するように変更
     
    ## ⏸ What's not done
    
    - React Native Reanimated 2の機能を使うためのBabel設定は含みません
      - Remote JS Debuggingが使えなくなるため

--- SantokuApp/index.js
+++ SantokuApp/index.js
@@ -1,12 +1,7 @@
 import 'react-native-gesture-handler';
 import {registerRootComponent} from 'expo';
-import {enableScreens} from 'react-native-screens';
 
 import {App} from './src/App';
-
-// https://reactnavigation.org/docs/react-native-screens
-// Enable screens support before rendering any of navigation stack
-enableScreens();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
