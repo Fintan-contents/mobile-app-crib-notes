8b1964dd853b3917bbd00b8298cfb3f2b4d5cfea
OshitaTatsuya <67449942+OshitaTatsuya@users.noreply.github.com>
2021-06-10 18:16:35 +0900

    🖋: ToDoの完了状態更新の改善コンテンツ (#265)
    ## ✅ What's done
    
    - [x] REST API呼び出し前に変更内容を即時更新する方法を説明
    - [x] 即時更新するよう修正した結果の動作確認方法を説明
    - [x] 紹介コードを利用しての動作確認

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -174,6 +174,7 @@ module.exports = {
             'react-native/learn/todo-app/networking/generate-api-client',
             'react-native/learn/todo-app/networking/api-request',
             'react-native/learn/todo-app/networking/activity-indicator',
+            'react-native/learn/todo-app/networking/improve-usability',
           ],
         },
       ],
