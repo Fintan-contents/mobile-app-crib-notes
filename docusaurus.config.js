1ad76b68b5d086bb102720ec506195c3e7b67d7f
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-06-10 16:58:30 +0900

    🚸: Fintanロゴからのリンク先をfintan.jpに変更し、faviconはこのサイトに含めるように修正 (#266)
    ## 💣 BREAKING CHANGES 💣
    
    - Fintanロゴからのリンクを、トップではなくfintan.jpに変更
    
    ## ✅ What's done
    
    - [x] Fintanロゴからのリンクを、トップではなくfintan.jpに変更
    - [x] favicon、Fintanロゴはfintan.jpのものを参照するのではなく、このサイトに含めるように修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ module.exports = {
   baseUrl,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'https://fintan.jp/wp-content/themes/fintan-luxeritas-child-theme_2.70/assets/favicon.ico',
+  favicon: 'img/favicon.ico',
   organizationName: organization, // Usually your GitHub org/user name.
   projectName: 'mobile-app-crib-notes', // Usually your repo name.
   noIndex: isDraft,
@@ -61,11 +61,12 @@ module.exports = {
       additionalLanguages: ['ruby', 'groovy'],
     },
     navbar: {
-      title: 'Fintan » Mobile App',
+      title: '» Mobile App',
       hideOnScroll: true,
       logo: {
         alt: 'Fintan Logo',
-        src: 'https://fintan.jp/wp-content/themes/fintan-luxeritas-child-theme_2.70/assets/favicon.ico',
+        src: 'img/fintan-logo-long.svg',
+        href: 'https://fintan.jp',
       },
       items: [
         {
