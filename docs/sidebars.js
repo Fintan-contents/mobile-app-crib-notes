9b5fd65e76a2ef129aedc0a7de8e627ffd1105c8
OshitaTatsuya <67449942+OshitaTatsuya@users.noreply.github.com>
2021-06-30 17:43:15 +0900

    🐛: 開発環境の構築ページから補足事項部分を切り出し (#340)
    * 修正 開発環境の構築ページから補足事項部分の切り出し
    
    * 修正 レビュー指摘対応（なにに対する補足かがわかるよう明記）

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -94,6 +94,7 @@ module.exports = {
         'react-native/learn/getting-started/setting-up-development-environment',
         'react-native/learn/getting-started/create-project',
         'react-native/learn/getting-started/launch-created-app',
+        'react-native/learn/getting-started/supplement',
       ],
     },
     {
