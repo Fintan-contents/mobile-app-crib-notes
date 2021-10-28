72774ad800b67960e7921d48ff8f6a82c08af2fb
a-mtn595COMPETIZIONE <90025233+a-mtn595COMPETIZIONE@users.noreply.github.com>
2021-10-28 16:32:49 +0900

    📝: サンプルアプリの機能要件・非機能要件を追加 (#483)
    ## ✅ What's done
    - [x] 要求一覧を機能要件の概要としてマージ
    - [x] 非機能要件（システム、ユーザビリティ、テスト）マージ
    - [x] サイドメニューに機能要件、非機能要件追加
    - [x] ページリンクをマージに合わせて修正
    
    <!-- 該当するものがなければ、このセクション（この行から「---」の前の行まで）を削除してください。 -->
    ## ⏸ What's not done
    
    - 非機能要件のシステム、ユーザビリティ、テスト以外のページをマージ
    - 全体の体裁を既存ドキュメントレベルへの見直し

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -225,6 +225,33 @@ module.exports = {
     },
     {
       type: 'category',
+      label: 'Requirements',
+      items: [
+        {
+          type: 'doc',
+          id: 'react-native/santoku/requirements',
+        },
+        {
+          type: 'category',
+          label: '機能要件',
+          items: [
+            'react-native/santoku/requirements/functional/overview'
+         ],
+        },
+        {
+          type: 'category',
+          label: '非機能要件',
+          items: [
+            'react-native/santoku/requirements/non-functional/overview',
+            'react-native/santoku/requirements/non-functional/usability',
+            'react-native/santoku/requirements/non-functional/system-method',
+            'react-native/santoku/requirements/non-functional/test-outline',
+         ],
+        },
+      ]
+    },
+    {
+      type: 'category',
       label: 'Application Architecture',
       items: [
         {
