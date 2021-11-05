a9ca96ebb6d317e2a545e4f39e0c5e4ef4e3c10e
takahashi <kz-takahashi@users.noreply.github.com>
2021-11-05 17:56:48 +0900

    📝: コンテキスト設計のドキュメント作成 (#528)
    ## ✅ What's done
    
    - [x] コンテキスト設計に関する以下のページを作成し、サイドバーに追加
      - [x] コンテキスト設計/概要
      - [x] コンテキスト設計/メッセージコンテキスト
      - [x] コンテキスト設計/テーマコンテキスト

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -316,6 +316,11 @@ module.exports = {
           type: 'doc',
           id: 'react-native/santoku/design/screen-transitions/overview',
         },
+        {
+          type: 'category',
+          label: 'コンテキスト設計',
+          items: ['react-native/santoku/design/context/overview', 'react-native/santoku/design/context/theme'],
+        },
       ],
     },
     {
