1e57eaeed0bf82fe06fad1d171fc15d90cb98195
In-a-Paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-11-09 19:04:50 +0900

    💄: スプラッシュスクリーンを追加 (#526)
    ## ✅ What's done
    
    - [x] `expo-splash-screen`でスプラッシュスクリーンを表示するように修正
    - [x] スプラッシュスクリーン用の画像を追加
    - [x] Androidのスプラッシュスクリーンとして表示するdrawableで`gravity`を利用できるようにパッチ適用
    - [x] Androidで表示する際にスプラッシュスクリーンが背景に突然現れることがないように、Activityを分離
    - [x] iOS用のスプラッシュスクリーンとして利用しなくなったxibを削除
    - [x] Androidでスプラッシュスクリーンがきれいに表示されるように調整
    - [x] 最初の画面(利用規約同意)で`expo-splash-screen`を非表示にする
    
    ## ⏸ What's not done
    
    - Activityを分割したが、Deeplinkが期待した通りに動作するかの検証は未着手

--- example-app/SantokuApp/index.js
+++ example-app/SantokuApp/index.js
@@ -1,7 +1,13 @@
 import 'react-native-gesture-handler';
 import {registerRootComponent} from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
 
 import {App} from './src/App';
+
+// Manually hide splash screen
+SplashScreen.preventAutoHideAsync().catch(() => {
+  console.info('Failed to prevent auto hide splash screen.');
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
--- example-app/SantokuApp/src/screens/account/TermsOfServiceAgreementScreen.tsx
+++ example-app/SantokuApp/src/screens/account/TermsOfServiceAgreementScreen.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React, {useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Text} from 'react-native-elements';
 import {useNavigateToAuthenticatedStackScreen} from 'screens/useNavigateToScreen';
@@ -6,6 +7,18 @@ import {useNavigateToAuthenticatedStackScreen} from 'screens/useNavigateToScreen
 const ScreenName = 'TermsOfServiceAgreement';
 const Screen: React.FC = () => {
   const onGoToHomeScreen = useNavigateToAuthenticatedStackScreen('Home');
+
+  // TODO: 本来は起動直後の初期化処理完了タイミングに持っていくべきなので、初期化処理実装時に併せて対応する
+  useEffect(() => {
+    // スプラッシュスクリーンを閉じる前に100ms程度は待たないと、コンポーネントのレンダリングが完了せず画面が一瞬白くなってしまいます。
+    // expo-app-loadingでも200ms待っているので、ここでも200ms待つようにしています。
+    // cf: https://github.com/expo/expo/blob/sdk-41/packages/expo-app-loading/src/AppLoadingNativeWrapper.tsx#L27
+    setTimeout(() => {
+      SplashScreen.hideAsync().catch(() => {
+        /* Ignore error */
+      });
+    }, 200);
+  }, []);
 
   return (
     <View style={styles.container} testID="TermsOfServiceAgreementScreen">
