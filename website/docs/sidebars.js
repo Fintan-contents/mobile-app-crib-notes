7d2fa1f3a729c1677bf3a7abb7f8ed8b4361c0f8
a-mtn595COMPETIZIONE <90025233+a-mtn595COMPETIZIONE@users.noreply.github.com>
2021-11-11 16:11:30 +0900

    📝: サンプルアプリの非機能要件「セキュリティに関する事項」を追加 (#540)
    ## ✅ What's done
    
    - [x] 非機能要件「セキュリティに関する事項」を追加
    - [x] サイドメニューおよび概要ページに当ページへのリンク追加
    
    <!-- 該当するものがなければ、このセクション（この行から「---」の前の行まで）を削除してください。 -->
    ## ⏸ What's not done
    
    - 全体の体裁を既存ドキュメントレベルへの見直し
    
    ---

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -249,6 +249,7 @@ module.exports = {
             'react-native/santoku/requirements/non-functional/system-method',
             'react-native/santoku/requirements/non-functional/performance',
             'react-native/santoku/requirements/non-functional/compatibility',
+            'react-native/santoku/requirements/non-functional/security',
             'react-native/santoku/requirements/non-functional/test-outline',
             'react-native/santoku/requirements/non-functional/operation',
           ],
