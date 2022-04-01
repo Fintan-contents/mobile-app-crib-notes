a7e9f52ef6abfe7f7b6096e45444f1a1cd61ef85
In-a-Paddy <44867563+in-a-paddy@users.noreply.github.com>
2022-04-01 16:04:15 +0900

    🩹: Refine ESLint config (#808)
    ## ✅ What's done
    
    - [x] Merge `.eslintignore` into `.eslintrc.js`
    - [x] Remove unnecessary configuration
    - [x] Disable `@typescript-eslint/no-misused-promises` in JSX

--- example-app/SantokuApp/.eslintrc.js
+++ example-app/SantokuApp/.eslintrc.js
@@ -1,23 +1,24 @@
+// To show all ESLint rules, run the following command.
+// npx eslint --print-config src/App.tsx
+
 module.exports = {
   root: true,
   // universe/native: https://github.com/expo/expo/tree/master/packages/eslint-config-universe
-  // react-hooks: https://ja.reactjs.org/docs/hooks-rules.html
-  extends: ['universe/native', 'plugin:react-hooks/recommended'],
+  extends: ['universe/native'],
   overrides: [
     {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
       // universe/shared/typescript-analysis: https://github.com/expo/expo/tree/master/packages/eslint-config-universe
       // recommended-requiring-type-checking: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
       extends: ['universe/shared/typescript-analysis', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
-      files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
         project: './tsconfig.json',
       },
     },
     {
+      files: ['*.test.ts', '*.test.tsx'],
       // https://github.com/jest-community/eslint-plugin-jest#shareable-configurations
       extends: ['plugin:jest/recommended'],
-      files: ['*.test.ts', '*.test.tsx'],
-      plugins: ['jest'],
       rules: {
         // you should turn the original rule off *only* for test files
         // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md
@@ -25,5 +26,20 @@ module.exports = {
         'jest/unbound-method': 'error',
       },
     },
+    {
+      files: ['*.tsx'],
+      rules: {
+        // It is too strict to prohibit passing async functions to `onPress`, so disable the rule in JSX.
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksVoidReturn: {
+              attributes: false,
+            },
+          },
+        ],
+      },
+    },
   ],
+  ignorePatterns: ['src/generated/**/*.ts'],
 };
