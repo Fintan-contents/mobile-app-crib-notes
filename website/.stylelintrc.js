c387f28724fbe0df953f96fd957f226f54ae5dce
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-05-17 10:03:30 +0900

    🚨: stylelintエラーが発生していたので対応（media-feature-range-notationをoffにする） (#1142)
    ## ✅ What's done
    
    `master`ブランチで以下のエラーが発生。
    ```console
    エラー内容：
    src/pages/styles.module.css
     15:19  ✖  Expected "context" media feature range notation  media-feature-range-notation
    
    src/css/custom.css
     36:8  ✖  Expected "context" media feature range notation  media-feature-range-notation
    ```
    
    - [x] Safariが最新の16.4(2023/03/27)しか対応してないので`media-feature-range-notation`をoffにする

--- website/.stylelintrc.js
+++ website/.stylelintrc.js
@@ -2,5 +2,8 @@ module.exports = {
   "extends": "stylelint-config-standard",
   "rules": {
     "selector-class-pattern": null,
+    // Safariが16.4以降（2023/03/27リリース）にしか対応していないので一旦無効化
+    // https://caniuse.com/css-media-range-syntax
+    "media-feature-range-notation": null,
   }
 }
